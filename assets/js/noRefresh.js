4ecf12be33f8d559e4c59f4f3d194edb9c3b5a1e
tiagoBatschke <83837600+tiagoBatschke@users.noreply.github.com>
2023-06-07 11:06:23 -0300

    att

--- assets/js/noRefresh.js
+++ assets/js/noRefresh.js
@@ -1,19 +1,10 @@
-
-
-function openPage(x, y) {
-    var indice = x
-    var target = y
-    var url = './assets/html/'+ indice +'.html'
-
-    var xml = new XMLHttpRequest()
-
-    xml.onreadystatechange = function () {
-        if (xml.readyState == 4 && xml.status == 200) {
-            document.getElementById(target).innerHTML = xml.responseText
-        }
+function openPage(id, ids) {
+    for (let i = 0; i < ids.length; i++) {
+        const element = ids[i];
+        console.log(element)
+        document.getElementById(element).classList.remove('hidden')
+        document.getElementById(element).classList.add('hidden')
     }
 
-    xml.open("GET", url, true)
-
-    xml.send()
+    document.getElementById(id).classList.toggle('hidden')
 }
