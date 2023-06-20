3bd3130047117ceb1b2d458444593447a5d172e5
tiagoBatschke <83837600+tiagoBatschke@users.noreply.github.com>
2023-06-20 12:53:03 -0300

    att

---
+++ assets/js/slick.js
@@ -0,0 +1,37 @@
+
+$(document).ready(function() {
+    $(".regular").slick({
+      dots: true,
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      appendArrows: $(".custom-arrows"),
+      prevArrow: '<div class="container_prev"><button class="slick-prev">‹</button></div>',
+      nextArrow: '<div class="container_next"><button class="slick-next">›</button></div>',
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 980,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+  });
