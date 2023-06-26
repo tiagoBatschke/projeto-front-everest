ff65353a5b32d84dec1845dce127361fc21c828e
tiagoBatschke <83837600+tiagoBatschke@users.noreply.github.com>
2023-06-26 16:43:33 -0300

    att

--- assets/js/noRefresh.js
+++ assets/js/noRefresh.js
@@ -36,13 +36,3 @@ function openListMessages(idLista, idButton, idsButtons, ids) {
   document.getElementById(idButton).classList.toggle("button_evidencia");
 }
 
-function openPage(id, ids) {
-  for (let i = 0; i < ids.length; i++) {
-    const element = ids[i];
-    console.log(element);
-    document.getElementById(element).classList.remove("hidden");
-    document.getElementById(element).classList.add("hidden");
-  }
-
-  document.getElementById(id).classList.toggle("hidden");
-}
