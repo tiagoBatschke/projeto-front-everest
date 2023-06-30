f2439cfc0c398ddf4fdad8d492b4c8a3c4e9d6a1
tiagoBatschke <83837600+tiagoBatschke@users.noreply.github.com>
2023-06-30 16:04:58 -0300

    att

--- assets/js/noRefresh.js
+++ assets/js/noRefresh.js
@@ -73,3 +73,26 @@ function openListMessages(idLista, idButton, idsButtons, ids) {
   document.getElementById(idLista).classList.toggle("hidden");
   document.getElementById(idButton).classList.toggle("button_evidencia");
 }
+
+
+function openListCrm(idLista, idButton, idsButtons, ids) {
+  for (let i = 0; i < ids.length; i++) {
+    const element = ids[i];
+    console.log(element);
+    document.getElementById(element).classList.remove("hidden");
+    document.getElementById(element).classList.add("hidden");
+  }
+
+  for (let i = 0; i < idsButtons.length; i++) {
+    const element = idsButtons[i];
+    console.log(element);
+    document.getElementById(element).classList.add("button_evidencia");
+    document.getElementById(element).classList.remove("button_evidencia");
+    document.getElementById(element).classList.add("card_perfil-text-whitea");
+    document.getElementById(element).classList.remove("card_perfil-text-white");
+  }
+
+  document.getElementById(idLista).classList.toggle("hidden");
+  document.getElementById(idButton).classList.toggle("button_evidencia");
+  document.getElementById(idButton).classList.toggle("card_perfil-text-white");
+}
